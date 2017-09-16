de049a44eb5b82b585474b73afcd660c432e828a
Thales Aguiar <thalesaguiar21@gmail.com>
2017-09-16 08:42:26 -0300

    Changed indentation size

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
 #include "utility/Vector3.h"
+#include "utility/Ray.h"
 
 using namespace utils;
 
 int main( int argc, char *argv[] ) {
-    Vector3 (1, 2, 3);
-    return 0;
+  /*Ray r (Point3(0, 0, 1), Point3(3, 1, 4));
+  std::cout << "Origin: " << r.origin << std::endl;
+  std::cout << "EndPoint: " << r.endPoint << std::endl;
+  std::cout << "Direction: " << r.Direction() << std::endl
+  std::cout << "PointAt: " << r.PointAt(2.0) << std::endl;*/
+  return 0;
 }
--- utility/Vector3.h
+++ utility/Vector3.h
@@ -6,45 +6,45 @@ Represents a basic tri-dimensional vector. can also be interpreted as a RGB
 color, a tri-dimensional Point in space
 */
 namespace utils {
-    class Vector3 {
+  class Vector3 {
     public:
-        typedef float element_type;
-        enum element { Cx=0, Cy=1, Cz=2, Cr=0, Cg=1, Cb=2 };
-        element_type e[3];
+      typedef float element_type;
+      enum element { Cx=0, Cy=1, Cz=2, Cr=0, Cg=1, Cb=2 };
+      element_type e[3];
 
-        // Constructors
-        Vector3 ( void );
-        Vector3(element_type x, element_type y, element_type z);
+      // Constructors
+      Vector3 ( void );
+      Vector3(element_type x, element_type y, element_type z);
 
-        // Operators overloading
-        element_type operator[]( size_t idx ) const;
-        element_type& operator[]( size_t idx );
-        Vector3& operator+=( const Vector3& );
-        Vector3& operator-=( const Vector3& );
-        Vector3& operator*=( const Vector3& );
-        Vector3& operator/=( const Vector3& );
-        Vector3& operator*=( const element_type );
-        Vector3& operator/=( const element_type );
-        Vector3& operator+(void);
-        Vector3 operator-(void);
+      // Operators overloading
+      element_type operator[]( size_t idx ) const;
+      element_type& operator[]( size_t idx );
+      Vector3& operator+=( const Vector3& );
+      Vector3& operator-=( const Vector3& );
+      Vector3& operator*=( const Vector3& );
+      Vector3& operator/=( const Vector3& );
+      Vector3& operator*=( const element_type );
+      Vector3& operator/=( const element_type );
+      Vector3& operator+(void);
+      Vector3 operator-(void);
 
-        // Accesors
-        element_type X();
-        element_type Y();
-        element_type Z();
-        element_type R();
-        element_type G();
-        element_type B();
+      // Accesors
+      element_type X();
+      element_type Y();
+      element_type Z();
+      element_type R();
+      element_type G();
+      element_type B();
 
-        // Methods
-        element_type Length() const;
-        element_type SquaredLength() const;
-        void MakeUnitVector();
-    };
+      // Methods
+      element_type Length() const;
+      element_type SquaredLength() const;
+      void MakeUnitVector();
+  };
 
-    typedef Vector3 RGB;
-    typedef Vector3 Offset;
-    typedef Vector3 Point3;
+  typedef Vector3 RGB;
+  typedef Vector3 Offset;
+  typedef Vector3 Point3;
 } // namespace utils
 
 #include "Vector3.inl"
