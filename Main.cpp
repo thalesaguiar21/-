752b05de472255c0e0b85983c1986867e056fa3b
Thales Aguiar <thalesaguiar21@gmail.com>
2017-10-27 10:14:31 -0300

    Rounded seconds in user feedback

--- Main.cpp
+++ Main.cpp
@@ -61,7 +61,7 @@ void ShowProgress(float num, float denom) {
 
 void PrintExecutionTime(float time) {
   int min = int(time / CLOCKS_PER_SEC) / 60;
-  float sec = (time / CLOCKS_PER_SEC) - (min * 60);
+  int sec = int(time / CLOCKS_PER_SEC) - (min * 60);
   cout << "\n------------ TIME ---------------" << endl;
   cout << "Rendered file for " << min << "min " << sec << "s" << endl;
   cout << "---------------------------------" << endl;
